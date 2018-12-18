d526430df042c4330100f41330a9ad5bc5319b83
onupamit <onupamit@gmail.com>
2018-12-18 12:01:46 -0500

    +Foe Loop Added

--- otherconatct/src/main/java/com/test/first/Testing1.java
+++ otherconatct/src/main/java/com/test/first/Testing1.java
@@ -1,8 +1,12 @@
 package com.test.first;
 
 public class Testing1 {
-    public static void main(String args[]){
-        System.out.println("Hello This is the 2nd ");
+    public static void main(String []args){
+
+        for(int i=0; i<10; i++ ){
+
+            System.out.println("Hello This is the 2nd ");
+        }
     }
 
 
